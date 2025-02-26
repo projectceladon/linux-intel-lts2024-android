a69a47ba5b23666bc9db4ab8c37be2eeeb02d5a5
Wei6 Zhang <wei6.zhang@intel.com>
2025-02-26 10:37:32 +0800

    driver: porting virtio_camera driver
    Tracked-On: OAM-130487
    Signed-off-by: Wei6 Zhang <wei6.zhang@intel.com>

--- drivers/media/v4l2-core/v4l2-dev.c
+++ drivers/media/v4l2-core/v4l2-dev.c
@@ -544,13 +544,17 @@ static void determine_valid_ioctls(struct video_device *vdev)
 			      V4L2_CAP_META_OUTPUT;
 	DECLARE_BITMAP(valid_ioctls, BASE_VIDIOC_PRIVATE);
 	const struct v4l2_ioctl_ops *ops = vdev->ioctl_ops;
-	bool is_vid = vdev->vfl_type == VFL_TYPE_VIDEO &&
+	bool is_vid = (vdev->vfl_type == VFL_TYPE_VIDEO ||
+			vdev->vfl_type == VFL_TYPE_UVC ||
+			vdev->vfl_type == VFL_TYPE_VIRTCAM) &&
 		      (vdev->device_caps & vid_caps);
 	bool is_vbi = vdev->vfl_type == VFL_TYPE_VBI;
 	bool is_radio = vdev->vfl_type == VFL_TYPE_RADIO;
 	bool is_sdr = vdev->vfl_type == VFL_TYPE_SDR;
 	bool is_tch = vdev->vfl_type == VFL_TYPE_TOUCH;
-	bool is_meta = vdev->vfl_type == VFL_TYPE_VIDEO &&
+	bool is_meta = (vdev->vfl_type == VFL_TYPE_VIDEO ||
+			vdev->vfl_type == VFL_TYPE_UVC ||
+			vdev->vfl_type == VFL_TYPE_VIRTCAM) &&
 		       (vdev->device_caps & meta_caps);
 	bool is_rx = vdev->vfl_dir != VFL_DIR_TX;
 	bool is_tx = vdev->vfl_dir != VFL_DIR_RX;
@@ -820,6 +824,8 @@ static int video_register_media_controller(struct video_device *vdev)
 
 	switch (vdev->vfl_type) {
 	case VFL_TYPE_VIDEO:
+	case VFL_TYPE_VIRTCAM:
+	case VFL_TYPE_UVC:
 		intf_type = MEDIA_INTF_T_V4L_VIDEO;
 		vdev->entity.function = MEDIA_ENT_F_IO_V4L;
 		break;
@@ -927,6 +933,12 @@ int __video_register_device(struct video_device *vdev,
 
 	/* Part 1: check device type */
 	switch (type) {
+	case VFL_TYPE_VIRTCAM:
+		name_base = "virtvideo";
+		break;
+	case VFL_TYPE_UVC:
+		name_base = "uvcvideo";
+		break;
 	case VFL_TYPE_VIDEO:
 		name_base = "video";
 		break;
@@ -972,6 +984,8 @@ int __video_register_device(struct video_device *vdev,
 	 * (new style). */
 	switch (type) {
 	case VFL_TYPE_VIDEO:
+	case VFL_TYPE_VIRTCAM:
+	case VFL_TYPE_UVC:
 		minor_offset = 0;
 		minor_cnt = 64;
 		break;
