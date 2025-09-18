f0b28db86cd647f0bdcc56ea4a14b2328a756e8e
Sun, Jing <jing.a.sun@intel.com>
2025-09-18 08:50:17 +0000

    Revert NPU FW5.0 Kernel Driver upgrading patches
    From Commit df8ec74f08b67ba460d2487e096440dce6688b8f
    to Commit 1f3bdedb0eec6e63d11328b5737a2f43a9c6287f
    
    Tracked-On: OAM-134080
    Signed-off-by: Sun, Jing <jing.a.sun@intel.com>

--- drivers/accel/ivpu/ivpu_drv.h
+++ drivers/accel/ivpu/ivpu_drv.h
@@ -21,6 +21,7 @@
 
 #define DRIVER_NAME "intel_vpu"
 #define DRIVER_DESC "Driver for Intel NPU (Neural Processing Unit)"
+#define DRIVER_DATE "20230117"
 
 #define PCI_DEVICE_ID_MTL	0x7d1d
 #define PCI_DEVICE_ID_ARL	0xad1d
@@ -46,21 +47,16 @@
 #define IVPU_MIN_DB 1
 #define IVPU_MAX_DB 255
 
-#define IVPU_JOB_ID_JOB_MASK		GENMASK(7, 0)
-#define IVPU_JOB_ID_CONTEXT_MASK	GENMASK(31, 8)
-
+#define IVPU_NUM_ENGINES       2
 #define IVPU_NUM_PRIORITIES    4
-#define IVPU_NUM_CMDQS_PER_CTX (IVPU_NUM_PRIORITIES)
+#define IVPU_NUM_CMDQS_PER_CTX (IVPU_NUM_ENGINES * IVPU_NUM_PRIORITIES)
 
-#define IVPU_CMDQ_MIN_ID 1
-#define IVPU_CMDQ_MAX_ID 255
+#define IVPU_CMDQ_INDEX(engine, priority) ((engine) * IVPU_NUM_PRIORITIES + (priority))
 
 #define IVPU_PLATFORM_SILICON 0
 #define IVPU_PLATFORM_SIMICS  2
 #define IVPU_PLATFORM_FPGA    3
 #define IVPU_PLATFORM_INVALID 8
-
-#define IVPU_SCHED_MODE_AUTO -1
 
 #define IVPU_DBG_REG	 BIT(0)
 #define IVPU_DBG_IRQ	 BIT(1)
@@ -139,8 +135,6 @@ struct ivpu_device {
 	struct xa_limit context_xa_limit;
 
 	struct xarray db_xa;
-	struct xa_limit db_limit;
-	u32 db_next;
 
 	struct mutex bo_list_lock; /* Protects bo_list */
 	struct list_head bo_list;
@@ -159,7 +153,6 @@ struct ivpu_device {
 		int tdr;
 		int autosuspend;
 		int d0i3_entry_msg;
-		int state_dump_msg;
 	} timeout;
 };
 
@@ -171,15 +164,11 @@ struct ivpu_file_priv {
 	struct kref ref;
 	struct ivpu_device *vdev;
 	struct mutex lock; /* Protects cmdq */
-	struct xarray cmdq_xa;
+	struct ivpu_cmdq *cmdq[IVPU_NUM_CMDQS_PER_CTX];
 	struct ivpu_mmu_context ctx;
 	struct mutex ms_lock; /* Protects ms_instance_list, ms_info_bo */
 	struct list_head ms_instance_list;
 	struct ivpu_bo *ms_info_bo;
-	struct xa_limit job_limit;
-	u32 job_id_next;
-	struct xa_limit cmdq_limit;
-	u32 cmdq_id_next;
 	bool has_mmu_faults;
 	bool bound;
 	bool aborted;
@@ -197,9 +186,9 @@ extern bool ivpu_force_snoop;
 #define IVPU_TEST_MODE_NULL_SUBMISSION    BIT(2)
 #define IVPU_TEST_MODE_D0I3_MSG_DISABLE   BIT(4)
 #define IVPU_TEST_MODE_D0I3_MSG_ENABLE    BIT(5)
-#define IVPU_TEST_MODE_MIP_DISABLE        BIT(6)
+#define IVPU_TEST_MODE_PREEMPTION_DISABLE BIT(6)
+#define IVPU_TEST_MODE_HWS_EXTRA_EVENTS	  BIT(7)
 #define IVPU_TEST_MODE_DISABLE_TIMEOUTS   BIT(8)
-#define IVPU_TEST_MODE_TURBO		  BIT(9)
 extern int ivpu_test_mode;
 
 struct ivpu_file_priv *ivpu_file_priv_get(struct ivpu_file_priv *file_priv);
--- drivers/accel/ivpu/ivpu_mmu_context.c
+++ drivers/accel/ivpu/ivpu_mmu_context.c
@@ -612,21 +612,17 @@ int ivpu_mmu_reserved_context_init(struct ivpu_device *vdev)
 	if (!ivpu_mmu_ensure_pgd(vdev, &vdev->rctx.pgtable)) {
 		ivpu_err(vdev, "Failed to allocate root page table for reserved context\n");
 		ret = -ENOMEM;
-		goto err_ctx_fini;
+		goto unlock;
 	}
 
 	ret = ivpu_mmu_cd_set(vdev, vdev->rctx.id, &vdev->rctx.pgtable);
 	if (ret) {
 		ivpu_err(vdev, "Failed to set context descriptor for reserved context\n");
-		goto err_ctx_fini;
+		goto unlock;
 	}
 
+unlock:
 	mutex_unlock(&vdev->rctx.lock);
-	return ret;
-
-err_ctx_fini:
-	mutex_unlock(&vdev->rctx.lock);
-	ivpu_mmu_context_fini(vdev, &vdev->rctx);
 	return ret;
 }
 
